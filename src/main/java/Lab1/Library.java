d32b9b257cd4e2c8970af418b9ec5616d1266256
yrchengdylan <56997585+yrchengdylan@users.noreply.github.com>
2021-02-16 16:25:28 +0800

    I have made a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -5,7 +5,9 @@ package Lab1;
 
 public class Library {
     public static void main(String arg[]) {
-    	System.out.println("Hello, world!");
+    	Library lib = new Library();
+    	if(lib.someLibraryMethod())
+    		System.out.println("Hello, world!");
     }
 	
 	public boolean someLibraryMethod() {
